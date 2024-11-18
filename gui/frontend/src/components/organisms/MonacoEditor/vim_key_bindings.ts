61c3d1cd1be4389fc0b6da805f1f48648dc09222
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-11-18 15:04:21 +0900

    feat(front): add `gx` vim key

--- gui/frontend/src/components/organisms/MonacoEditor/vim_key_bindings.ts
+++ gui/frontend/src/components/organisms/MonacoEditor/vim_key_bindings.ts
@@ -1,6 +1,8 @@
+import { start } from '../../../services/api/shell';
+
+import type { MonacoEditor, VimModeRef, VimStatusRef } from './MonacoEditor';
 import type MonacoVim from 'monaco-vim';
 import type { Vim } from 'monaco-vim';
-import type { MonacoEditor, VimModeRef, VimStatusRef } from './MonacoEditor';
 
 const defineVimExCommand = (
   vim: Vim,
@@ -16,6 +18,18 @@ const defineVimExCommand = (
   vim.map(key, `:${exCommand}`, mode);
 };
 
+const getWordAtCursor = (editor: MonacoEditor) => {
+  const position = editor.getPosition();
+  if (!position) {
+    return;
+  }
+  const wordInfo = editor.getModel()?.getWordAtPosition(position);
+  if (wordInfo) {
+    const word = wordInfo.word;
+    return word;
+  }
+};
+
 const setCustomVimKeyConfig = (editor: MonacoEditor, vim: Vim) => {
   for (const key of ['jj', 'jk', 'kj'] as const) {
     vim.map(key, '<Esc>', 'insert');
@@ -24,6 +38,14 @@ const setCustomVimKeyConfig = (editor: MonacoEditor, vim: Vim) => {
   // Fix the problem that the default `%` is one-way and we can't go back.
   defineVimExCommand(vim, 'goToBracket', editor, 'editor.action.jumpToBracket', '%', 'normal');
   defineVimExCommand(vim, 'showHover', editor, 'editor.action.showHover', 'K', 'normal');
+
+  vim.defineEx('GoTo', 'GoTo', async () => {
+    const url = getWordAtCursor(editor);
+    if (url) {
+      await start(url);
+    }
+  });
+  vim.map('gx', ':Goto', 'normal');
 };
 
 type VimKeyLoader = (props: { editor: MonacoEditor; vimModeRef: VimModeRef; vimStatusRef: VimStatusRef }) => void;
