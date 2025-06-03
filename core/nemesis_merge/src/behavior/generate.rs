1bafa68942483c81f5e83b4634bd50270a277f00
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-06-03 21:01:43 +0900

    feat: enhance tracing to log id_order sorted by priority

--- core/nemesis_merge/src/behavior/generate.rs
+++ core/nemesis_merge/src/behavior/generate.rs
@@ -21,7 +21,11 @@ use std::path::PathBuf;
 pub async fn behavior_gen(nemesis_paths: Vec<PathBuf>, config: Config) -> Result<()> {
     let id_order = paths_to_priority_map(&nemesis_paths);
     #[cfg(feature = "tracing")]
-    tracing::trace!("id_order = {id_order:#?}");
+    {
+        let mut sorted: Vec<_> = id_order.par_iter().collect();
+        sorted.par_sort_by_key(|&(_, v)| *v);
+        tracing::trace!("id_order_by_priority = {sorted:#?}");
+    }
 
     // Collect all patches file.
     config.on_report_status(Status::ReadingTemplatesAndPatches);
