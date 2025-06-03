73919106c9a62bd476926ae8cd731cb0578f8792
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-06-03 21:07:42 +0900

    refactor: rename priority retrieval function for clarity

--- core/nemesis_merge/src/patches/collect.rs
+++ core/nemesis_merge/src/patches/collect.rs
@@ -41,13 +41,13 @@ pub async fn collect_owned_patches(
     let mut handles = vec![];
     let paths = nemesis_paths.iter().flat_map(collect_nemesis_paths);
 
-    fn get_priority(path: &Path, ids: &PriorityMap<'_>) -> Option<usize> {
+    fn get_priority_by_path_id(path: &Path, ids: &PriorityMap<'_>) -> Option<usize> {
         let id_str = get_nemesis_id(path.to_str()?).ok()?;
         ids.get(id_str).copied()
     }
 
     for (category, path) in paths {
-        let priority = get_priority(&path, id_order).unwrap_or(usize::MAX); // todo error handling
+        let priority = get_priority_by_path_id(&path, id_order).unwrap_or(usize::MAX); // todo error handling
 
         handles.push(tokio::spawn(async move {
             let content = fs::read_to_string(&path)
