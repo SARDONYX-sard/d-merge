1bb417b47dba13cf61560b43b6a906b8fe5e7053
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-06-08 12:07:38 +0900

    fix: update write_file to ensure parent directory is created before writing file

--- gui/backend/src/cmd/fs.rs
+++ gui/backend/src/cmd/fs.rs
@@ -6,5 +6,12 @@ use std::path::Path;
 /// (there was a case that the order of some data in contents was switched).
 #[tauri::command]
 pub(crate) async fn write_file(path: &Path, content: &str) -> Result<(), String> {
-    std::fs::write(path, content).or_else(|err| bail!(err))
+    if let Some(parent) = path.parent() {
+        tokio::fs::create_dir_all(parent)
+            .await
+            .or_else(|err| bail!(err))?;
+    }
+    tokio::fs::write(path, content)
+        .await
+        .or_else(|err| bail!(err))
 }
