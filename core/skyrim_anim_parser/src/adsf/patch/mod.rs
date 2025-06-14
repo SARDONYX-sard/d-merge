4132e0d27c2bc1814aad71b4ae6f6b9ab253a1f2
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-06-15 05:50:24 +0900

    fix: remove duplicate module declaration for candidate in mod.rs

--- core/skyrim_anim_parser/src/adsf/patch/mod.rs
+++ core/skyrim_anim_parser/src/adsf/patch/mod.rs
@@ -1,9 +1,9 @@
+mod candidate;
 mod comment;
 mod current_state;
 mod error;
 mod old;
-mod candidate;
 
+pub use self::candidate::parse_adsf_patch;
 pub use self::old::parse_clip_anim_block_patch;
 pub use self::old::parse_clip_motion_block_patch;
-pub use self::candidate::parse_adsf_patch;
