58d74a36e75684eb9ebac85fceb60685a0e99fad
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-08-20 18:25:07 +0900

    chore: move vitest config file

--- gui/frontend/vitest.config.ts
+++ gui/frontend/vitest.config.ts
@@ -1,7 +1,3 @@
-// This file is a test configuration file for gui/frontend.
-// By placing the configuration file in the root directory, it eliminates wasted time in directory searches
-// and prevents time delays in testing.
-
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
@@ -9,9 +5,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
-    alias: [{ find: '@/', replacement: `${__dirname}/gui/frontend/src/` }],
+    alias: [{ find: '@/', replacement: `${__dirname}/src/` }],
     globals: true,
-    root: './gui/frontend/src/',
+    root: './src/',
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.mts', 'tests/vitest.customMatchers.ts'],
     testTransformMode: { ssr: ['**/*'] },
