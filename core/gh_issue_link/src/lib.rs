35937380f9c84d0337f86c8a580d03c20223f074
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-10-16 07:40:54 +0900

    feat: change os name

--- core/gh_issue_link/src/lib.rs
+++ core/gh_issue_link/src/lib.rs
@@ -83,7 +83,14 @@ impl<'a> EnvInfo<'a> {
             "{:.1} GB",
             sys.total_memory() as f64 / 1024.0 / 1024.0 / 1024.0
         );
-        let os = System::kernel_long_version();
+
+        let os = System::long_os_version();
+        let ver = System::kernel_version();
+        let os = format!(
+            "{} {}",
+            os.as_deref().unwrap_or("Unknown OS"),
+            ver.as_deref().unwrap_or("")
+        );
 
         Self { cpu, dram, os }
     }
