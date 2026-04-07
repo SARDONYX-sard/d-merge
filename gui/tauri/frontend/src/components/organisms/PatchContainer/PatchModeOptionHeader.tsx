8c4ac239b375849d7126c5000acdafcd40a8f5f2
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2026-04-08 01:44:43 +0900

    feat(tauri_front): fix virt to horizontal

--- gui/tauri/frontend/src/components/organisms/PatchContainer/PatchModeOptionHeader.tsx
+++ gui/tauri/frontend/src/components/organisms/PatchContainer/PatchModeOptionHeader.tsx
@@ -1,14 +1,14 @@
-import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
-import { Checkbox } from '@mui/material';
-import { useCallback } from 'react';
-import { usePatchContext } from '@/components/providers/PatchProvider';
+import { FormControl, FormControlLabel, FormGroup, Radio, RadioGroup } from "@mui/material";
+import { Checkbox } from "@mui/material";
+import { useCallback } from "react";
+import { usePatchContext } from "@/components/providers/PatchProvider";
 
 export const PatchModeOptionHeader = () => {
   const { isVfsMode, setIsVfsMode, patchOptions, setPatchOptions } = usePatchContext();
 
   const handleChange = useCallback(
     (_: React.ChangeEvent<HTMLInputElement>, value: string) => {
-      setIsVfsMode(value === 'vfs');
+      setIsVfsMode(value === "vfs");
     },
     [setIsVfsMode],
   );
@@ -22,14 +22,19 @@ export const PatchModeOptionHeader = () => {
 
   return (
     <FormControl>
-      <RadioGroup row value={isVfsMode ? 'vfs' : 'manual'} onChange={handleChange}>
-        <FormControlLabel value='vfs' control={<Radio />} label={'VFS'} />
-        <FormControlLabel value='manual' control={<Radio />} label={'Manual'} />
-      </RadioGroup>
-      <FormControlLabel
-        label='FNIS.esp'
-        control={<Checkbox checked={patchOptions.generateFnisEsp} onChange={handleGenerateFnisEsp} />}
-      />
+      <FormGroup row>
+        <RadioGroup row value={isVfsMode ? "vfs" : "manual"} onChange={handleChange}>
+          <FormControlLabel value="vfs" control={<Radio />} label={"VFS"} />
+          <FormControlLabel value="manual" control={<Radio />} label={"Manual"} />
+        </RadioGroup>
+
+        <FormControlLabel
+          label="FNIS.esp"
+          control={
+            <Checkbox checked={patchOptions.generateFnisEsp} onChange={handleGenerateFnisEsp} />
+          }
+        />
+      </FormGroup>
     </FormControl>
   );
 };
